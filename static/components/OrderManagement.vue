7ea4856871b225712c15b948254fc9910f28a3a5
wang2810231260 <wang15638715518@163.com>
2026-03-17 17:03:22 +0800

    测试jenkins自动化

--- static/components/OrderManagement.vue
+++ static/components/OrderManagement.vue
@@ -1,8 +1,6 @@
 <template>
     <section id="order-generation" class="content-section active">
         <h1 class="section-title">订单生成</h1>
-        <h1 class="section-title">test111111</h1>
-
         <div class="form-container">
             <div class="form-group">
                 <label>选择 APP</label>
